40d72d794ec0cb37c894fc3913130d14c7a73703
Samonac <samonac@hotmail.com>
2022-06-26 21:00:45 +0200

    added all kinds of stuf

--- launch.sh
+++ launch.sh
@@ -1,6 +1,8 @@
-pip install flask
-pip install render_template
+python ./get-pip.py
+python -m pip install --upgrade pip setuptools wheel
+pip -r requirements.txt
 
 start http://127.0.0.1:5000/
 python ./carsale.py
 sleep 1
+echo "Thanks for watching ! Going to sleep now"
